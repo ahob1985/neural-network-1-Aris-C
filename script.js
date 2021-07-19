a24c997ebd35941520ca24b5ecc6e7b785d97dd4
ArisC1 <67166970+Aris-C@users.noreply.github.com>
2021-07-19 17:58:26 +0000

    finished

--- script.js
+++ script.js
@@ -7,12 +7,41 @@ let textDiv;
 let textP;
 let buttonDiv;
 let trainButton;
+let radioDiv;
+let notesRadio;
 
 // Global ML Variables
-//let model;
+let model;
+let state;
+let env;
+let wave;
+let notes;
 
 function setup() {
-
+  canvasDiv = createDiv();
+  canvas = createCanvas(640, 480);
+  canvas.mousePressed(canvasClicked);
+  canvas.parent(canvasDiv);
+  textDiv = createDiv();
+  textP = createP("Step 1: Data Collection");
+  textP.parent(textDiv);
+  buildButtons();
+  state = "collection";
+  notes = {
+    C: 261.6256,
+    D: 293.6648,
+    E: 329.6276,
+    F: 349.2282,
+    G: 391.9954
+  };
+  let options = {
+    inputs: ["x", "y"],
+    outputs: ["label"],
+    task: "classification",
+    debug: true
+  };
+  model = ml5.neuralNetwork(options);
+  createMusicSystem();
 }
 
 function draw() {
@@ -20,6 +49,20 @@ function draw() {
 }
 
 function buildButtons() {
+  radioDiv = createDiv();
+  notesRadio = createRadio();
+  notesRadio.option("C");
+  notesRadio.option("D");
+  notesRadio.option("E");
+  notesRadio.option("F");
+  notesRadio.option("G");
+  notesRadio.selected("C");
+  notesRadio.parent(radioDiv);
+  buttonDiv = createDiv();
+  trainButton = createButton("Train Model");
+  trainButton.mousePressed(trainModel);
+  trainButton.parent(buttonDiv);
+
 
 }
 
@@ -35,25 +78,60 @@ function createMusicSystem() {
 }
 
 function trainModel() {
-
+  state = "training";
+  textP.html("Step 2: Training");
+  model.normalizeData();
+  let options = {
+    epochs: 100
+  };
+  model.train(options, whileTraining, finishedTraining);
+  buttonDiv.style("display", "none");
+  radioDiv.style("display", "none");
 }
 
 function whileTraining(epoch, loss) {
-
+  console.log(epoch);
 }
 
 function finishedTraining() {
-
+  state = "prediction";
+  textP.html("Step 3: Prediction");
 }
 
 function drawNote(note, noteColor, ellipseColor) {
-
+  stroke(0);
+  fill(ellipseColor);
+  ellipse(mouseX, mouseY, 24);
+  fill(noteColor);
+  noStroke();
+  textAlign(CENTER, CENTER);
+  text(note, mouseX, mouseY);
+  wave.freq(notes[note]);
+  env.play();
 }
 
 function canvasClicked() {
-
+  let inputs = {
+    x: mouseX,
+    y: mouseY
+  };
+  if(state === "collection") {
+    let targetLabel = notesRadio.value();
+    let target = {
+      label: targetLabel
+    };
+    model.addData(inputs, target);
+    drawNote(targetLabel, "black", "white");
+  } else if(state === "prediction") {
+    model.classify(inputs, gotResults);
+  }
 }
 
 function gotResults(error, results) {
-
+  if(error) {
+    console.error(error);
+  } else {
+    let label = results[0].label;
+    drawNote(label, "white", "blue");
+  }
 }
